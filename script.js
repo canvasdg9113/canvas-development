658e5c26e71e077928ef9a8a10a1e760807a7fc0
canvasdg9113 <vandymeg@gmail.com>
2026-09-19 08:45:17 -0400

    Add responsive navigation

---
+++ script.js
@@ -0,0 +1,1 @@
+const b=document.querySelector('.menu'),n=document.querySelector('.site-header nav');b?.addEventListener('click',()=>{const o=n.classList.toggle('open');b.setAttribute('aria-expanded',o)});
